2f4afd43861c1364379f2e0c43269ad1c59219eb
Filipe Colaquecez <filipepcolaquecez@gmail.com>
2022-04-11 08:51:05 -0300

    Initial Commit

---
+++ Spotify/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Spotify
+//
+//  Created by Filipe Pereira Colaquecez on 11/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
